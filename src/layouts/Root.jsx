c4f91b82b677f3ba5322e265e3fe89a3040102e3
AlaminHasanPro <alaminhasanpro@gmail.com>
2024-02-03 14:16:29 +0600

    navbar added in layout

--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import NavBar from "../Copmonents/Shared/Navbar";
 
 const Root = () => {
   return (
-    <div>
-      <Outlet />
+    <div className="overflow-x-hidden">
+      <div>
+        <NavBar />
+      </div>
+      <div className="md:min-h-[calc(100vh-100px)]">
+        <Outlet />
+      </div>
     </div>
   );
 };
